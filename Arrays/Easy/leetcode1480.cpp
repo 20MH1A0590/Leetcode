d702da23439d5cb251056417ea5654847673c44c
koppula_2004 <ravindrareddyk5248@gmail.com>
2023-02-25 22:15:00 +0530

    Running Sum of 1d Array

---
+++ Arrays/Easy/leetcode1480.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        // prefix algo
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            ans.push_back(nums[i]+ans[i-1]);
+        }
+        return ans;
+    }
+};
