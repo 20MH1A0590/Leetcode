a5cd8d9f7d0d02842ed997f8fc3c9e5d9b529778
ravindra0602 <ravindrareddyk5248@gmail.com>
2023-03-08 17:15:40 +0530

    Calculate Money in Leetcode Bank

---
+++ Math/Easy/leetcode1716.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+      // simple maths 
+      int start = 1,incr = 0,total_money = 0;
+      while(n--)
+      {
+          total_money = total_money+start+incr;
+          start++;
+          if(start%8 == 0)
+          {
+              start=1;
+              incr++;
+          }
+      }
+      return total_money;
+    }
+};
