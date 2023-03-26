f6b175f38d71a922d79c14a64d54c34e4d644105
ravindra0602 <ravindrareddyk5248@gmail.com>
2023-03-26 14:15:24 +0530

    Merge Strings Alternately

---
+++ two pointer/Easy/leetcode1768.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int s1 = word1.size();
+        int s2 = word2.size();
+        string ans = "";
+        int k = min(s1,s2);
+        int i = 0,j = 0;
+        while(k)
+        {
+            ans+=word1[i];
+            ans+=word2[j];
+            i++;
+            j++;
+            k--;
+        }
+        if(s1 > s2) ans += word1.substr(s2,s1);
+        else if(s1 < s2) ans += word2.substr(s1,s2);
+        return ans;
+    }
+};
